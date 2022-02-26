313b5db5d5283158805cab15fd5a92763019f13f
Kito <chocon2060@gmail.com>
2022-02-26 16:39:03 +0700

    Update Protocol conert

--- src/ProtocolConvert.cpp
+++ src/ProtocolConvert.cpp
@@ -1,1 +1,47 @@
 #include"ProtocolConvert.h"
+void ProtocolConvert::serverToloraConvert(String msgPayload){
+    lastpoint = msgPayload.indexOf("{") + 1;
+    nextpoint = msgPayload.indexOf(",");
+    payloadSub[0] =  msgPayload.substring(lastpoint,nextpoint);
+
+    lastpoint = payloadSub[0].indexOf('"') + 1;
+    nextpoint = payloadSub[0].indexOf('"',lastpoint);
+    payloadSub[1] = payloadSub[0].substring(lastpoint,nextpoint);
+    if(payloadSub[1] == "Client")
+    {
+        ProtocolConvert::serverClientCmd(msgPayload);
+    }
+    else if(payloadSub[1] == "Getway")
+    {
+        ProtocolConvert::serverGetwayCmd(msgPayload);
+    }
+}
+
+void ProtocolConvert::serverClientCmd(String msgPayload){
+    lastpoint = msgPayload.indexOf("{") + 1;
+    nextpoint = msgPayload.indexOf(",");
+    payloadSub[0] =  msgPayload.substring(lastpoint,nextpoint);
+
+    lastpoint = payloadSub[0].indexOf(':') + 1;
+    nextpoint = payloadSub[0].indexOf(',',lastpoint);
+    payloadSub[1] = payloadSub[0].substring(lastpoint,nextpoint);
+    // check string is number 
+    if(!ProtocolConvert::isNumber( payloadSub[1])){
+        return;
+    }
+    
+
+
+
+}
+
+bool ProtocolConvert::isNumber(String str){
+
+    for (int i = 0; i < str.length(); i++){
+        if (!isdigit(str[i]))
+        {
+            return false;
+        }
+    }
+    return true;    
+}
--- src/ProtocolConvert.h
+++ src/ProtocolConvert.h
@@ -1,7 +1,19 @@
 #ifndef ProtocolConvert_H
 #define ProtocolConvert_H
-class ProtocolConvert{
-
+#include <stdint.h>
+#include <Arduino.h>
+class ProtocolConvert {  
+    private:
+        uint8_t loraMsg[50];
+        uint8_t loraMsgLenght;
+        uint8_t lastpoint = 0;
+        uint8_t nextpoint = 0;
+        String payloadSub[3];
+    public:
+        void serverToloraConvert(String msgPayload);
+        void serverClientCmd(String msgPayload);
+        void serverGetwayCmd(String msgPayload);
+        bool isNumber(String str);
+        
 };
-
 #endif
