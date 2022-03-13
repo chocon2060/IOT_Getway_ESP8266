c16abc457ffe5a8901c59935acf12a7792db27e8
Kito <chocon2060@gmail.com>
2022-03-14 00:11:41 +0700

    Updata Json type

--- src/ProtocolConvert.h
+++ src/ProtocolConvert.h
@@ -2,22 +2,16 @@
 #define ProtocolConvert_H
 #include <stdint.h>
 #include <Arduino.h>
-#include"configuration.h"
-// #include"CRC32.h"
+#include <ArduinoJson.h>
 class ProtocolConvert {  
     private:
-        uint8_t loraMsg[50];
-        uint8_t loraMsgLenght;
-        uint8_t lastpoint = 0;
-        uint8_t nextpoint = 0;
-        String payloadSub[3];
-        // crc32 crc;
+        StaticJsonDocument<256> doc;
+        String cmd;
+        String dataAddress;
     public:
-        void serverToloraConvert(String msgPayload);
-        void serverClientCmd(String msgPayload);
-        void serverGetwayCmd(String msgPayload);
-        bool isNumber(String str);
-        // void ProtocolConverError(String Error);
-        
+        void configjson();
+        void Convert(String msgPayload);
+        void toClient(String jsonMsg);
+        void toGetway(String jsonMsg);
 };
 #endif
